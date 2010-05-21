77cd4a415e5ee23bc70b85786e11b6d7dee00db1
Litvinov Sergey <slitvinov@gmail.com>
2010-05-21 16:06:19 +0200

    Add tcl as a third party library

--- local-install.sh
+++ local-install.sh
@@ -5,18 +5,17 @@ set -u
 PREFIX=$(pwd)/prefix-thirdparty
 WRKDIR=$(pwd)
 
-# compile and install blitz
-cd blitz
-CXX=g++ ./configure --prefix=$PREFIX
-make install
-cd ${WRKDIR}
-
 # compile and install tcl
 cd tcl8.5.8/unix
 CC=gcc CXX=g++ ./configure --prefix=$PREFIX
 make install
 cd ${WRKDIR}
 
-cd ..
-./configure --with-blitz=${PREFIX}/blitz-prefix --with-tcl=${PREFIX}/lib/
+# compile and install blitz
+cd blitz
+CXX=g++ ./configure --prefix=$PREFIX
+make install
+cd ${WRKDIR}
+
+./configure --with-blitz=${PREFIX} --with-tcl=${PREFIX}/lib/
 make
