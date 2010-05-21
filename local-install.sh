ce1caa5145a49472189d07f1a68953b8bf7cc919
Litvinov Sergey <slitvinov@gmail.com>
2010-05-21 15:51:42 +0200

    Add tcl to the source

--- local-install.sh
+++ local-install.sh
@@ -2,9 +2,21 @@
 
 set -e
 set -u
+PREFIX=$(pwd)/prefix-thirdparty
+WRKDIR=$(pwd)
+
+# compile and install blitz
 cd blitz
-CXX=g++ ./configure --prefix=$(pwd)/blitz-prefix
+CXX=g++ ./configure --prefix=$PREFIX
 make install
+cd ${WRKDIR}
+
+# compile and install tcl
+cd tcl8.5.8/unix
+CC=gcc CXX=g++ ./configure --prefix=$PREFIX
+make install
+cd ${WRKDIR}
+
 cd ..
-./configure --with-blitz=$(pwd)/blitz/blitz-prefix
+./configure --with-blitz=${PREFIX}/blitz-prefix --with-tcl=${PREFIX}/lib/
 make
