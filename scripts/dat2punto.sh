0f0d341164961e9e0c1a9b312e277cf813076c38
Litvinov Sergey <vital303@vital303-laptop.(none)>
2010-06-24 00:36:37 +0200

    Fix changes in dat2punto

--- scripts/dat2punto.sh
+++ scripts/dat2punto.sh
@@ -3,7 +3,13 @@
 
 # punto -D 2 -c 4 -G 0:0.2 -s 10 hm 
 
-for file in $(ls -1 prtl[0-9]*.dat); do
+if [ -z "$1" ]; then
+    pat="prtl"
+else
+    pat="$1"
+fi
+
+for file in $(ls -1 ${pat}[0-9]*.*); do
     #printf "file: %s\n" $file > "/dev/stderr"
     awk 'NF>3&&NR>3{print $1, $2, $3, $4, $5, $6, $7}'  $file 
     printf "\n"
