81198a1d29ea4cbf35c5b0782c5e888c555009aa
Sergey Litvinov <slitvinov@gmail.com>
2010-06-28 11:04:39 +0200

    Add glbtype.h to the repos

---
+++ src/glbtype.h
@@ -0,0 +1,26 @@
+/// \file glbtype.h
+/// \brief Global types
+
+#ifndef GLBTYPE_H
+#define GLBTYPE_H
+
+#include <boost/smart_ptr/shared_ptr.hpp>
+#include <boost/numeric/ublas/matrix.hpp>
+
+class Material;
+class Particle;
+class Interaction;
+///a shared pointer to Material
+typedef boost::shared_ptr<Material> spMaterial;
+
+///a shared pointer to Particle
+typedef boost::shared_ptr<Particle> spParticle;
+
+///a shared pointer to Interaction
+typedef boost::shared_ptr<Interaction> spInteraction;
+
+namespace blast = boost::numeric::ublas;
+//a vector of shared_ptr to material
+typedef blast::vector< spMaterial > vecMaterial;
+
+#endif
