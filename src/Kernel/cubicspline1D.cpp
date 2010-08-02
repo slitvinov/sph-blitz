923deb22f8d238997e5fbfc3841ba64f60bc7e9b
Oliver <oliver@linux-ulm1.site>
2010-08-02 15:49:46 -0700

    cubicspline1D source files added to repository

---
+++ src/Kernel/cubicspline1D.cpp
@@ -0,0 +1,92 @@
+///\file cubicspline1D.cpp
+///\author Xiangyu Hu <Xiangyu.Hu@aer.mw.tum.de>
+///\author changes by: Martin Bernreuther <Martin.Bernreuther@ipvs.uni-stuttgart.de>, 
+
+//----------------------------------------------------------------------------------------
+//      A concrete kernel class
+//		see Monaghan & Lattanzio (1985).
+//		Most often used kernel.
+//		bskernel.cpp
+//----------------------------------------------------------------------------------------
+
+// ***** system includes *****
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+
+#include <math.h>
+
+//local includes
+#include "glbfunc.h"
+#include "Kernel/cubicspline1D.h"
+
+using namespace std;
+const double pi = 3.141592653589793238462643383279502884197;
+
+/// Cubic spline kernel (see Liu eq. (3.6)
+CubicSpline1D::CubicSpline1D(const double supportlength)
+  : Kernel(supportlength),
+    norm( 1.0/3.0  /supportlength ) ,
+    factorGradW( 2.0*norm /supportlength )
+{
+  // initialize the auxiliary factors
+
+ }
+//----------------------------------------------------------------------------------------
+// Calculates the kernel value for the given distance of two particles
+//----------------------------------------------------------------------------------------
+double CubicSpline1D::w(const double distance) const
+{
+  const double R= 2.0 * distance/ supportlength;
+  if(R>2.0)
+    {
+      //support of 4h (supportlength =2h, h=0.015), everything beyond is zero
+      return 0.0;
+    }
+  else if(R>1.0) {
+    const double s2 = 2.0 - R;
+    return norm * s2 * s2 * s2 ;
+  }
+  else
+    {
+      const double s2 = 2.0 - R;
+      const double s1 = 1.0 - R;
+      return norm * ( s2*s2*s2 - 4.0*s1*s1*s1 );
+    };
+}
+//----------------------------------------------------------------------------------------
+// Calculates the kernel derivation for the given distance of two particles
+// We take this from Morris, Fox and Zhu (1997)
+// but used a tripled smoothing length for the definition of the interaction radius.
+//----------------------------------------------------------------------------------------
+Vec2d CubicSpline1D::gradW(const double distance, const Vec2d& distanceVector) const
+{  
+  return CubicSpline1D::F(distance)*distanceVector;
+}
+//----------------------------------------------------------------------------------------
+//		Calculates the kernel derivation (a double not vector) to distance
+//----------------------------------------------------------------------------------------
+double CubicSpline1D::F(const double distance) const
+{
+  double R= 2.0*distance/ supportlength; // 
+  if (R>2.0) {
+    //support of 4h, everything beyond is zero    
+    return 0.0;
+  }
+  else if(R>1.0 ) {
+    const double s2 = 2.0 - R;
+    return/*minus inserted!!! */ - factorGradW * ( 3.0 * s2*s2);
+  } else  {
+    const double s1 = 1.0 - R;
+    const double s2 = 2.0 - R;
+    return /* -*/factorGradW * ( 12.0 * s1*s1 - 3.0 * s2 * s2 );
+  }
+}
+
+void CubicSpline1D::show_information() const {
+  std::cerr << "(kernel) CubicSpline1D\n";
+}
---
+++ src/Kernel/cubicspline1D.h
@@ -0,0 +1,60 @@
+#ifndef CUBICSPLINE1D_H
+#define CUBICSPLINE1D_H
+/// \file CubicSpline1D.h 
+/// \brief  cubic spline kernel 1D
+
+/// CubicSpline class
+
+#include "Kernel/kernel.h"
+
+class CubicSpline1D : public Kernel
+{
+
+public:
+
+  ///constructor to initialize the data members (auxiliary factors for kernel calculation)
+  CubicSpline1D(const double supportlength);
+  
+  ///\brief Calculates the kernel value for the given distance of two particles
+  ///
+  /// We take this from Morris, Fox and Zhu (1997)
+  /// but used a tripled smoothing length for the definition of the interaction radius. 
+  virtual double w(const double distance) const;
+  
+  ///\brief Calculates the kernel derivation  for the given distance of two particles 
+  ///
+  /// We take this from Morris, Fox and Zhu (1997)
+  /// but used a tripled smoothing length for the definition of the interaction radius.
+  virtual Vec2d gradW(const double distance, const Vec2d& distanceVector) const;
+  
+  ///Calculates the kernel derivation (a double, not a vector) to distance
+  double F(const double distance) const;
+
+  ///Calculates the kernel Laplacian
+  double LapW(const double distance) const;
+
+  /// show the kernel type
+  void show_information() const;
+
+private:
+    ///smoothing length
+    double h;
+
+    ///Normalization factor
+    const double norm;
+
+    ///Auxiliary factors for intermediate results: The inverse smoothing length
+    double reciprocH;
+
+    ///Auxiliary factors for intermediate results: A pre-factor for w 
+    double factorW;
+
+    ///Auxiliary factors for intermediate results: A pre-factor for grad w
+    double factorGradW;
+
+    ///Auxiliary factors for Laplacian
+    double factorLapW;
+
+};
+
+#endif //CUBICSPLINE1D_H
