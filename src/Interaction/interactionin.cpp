49f1860f58b1c80a83dd85f6274109b5f1a80342
Litvinov Sergey <slitvinov@gmail.com>
2010-10-03 14:13:34 +0200

    Small correction of code formating in interactionin.cpp

--- src/Interaction/interactionin.cpp
+++ src/Interaction/interactionin.cpp
@@ -23,28 +23,21 @@
 void InteractionIn::UpdateForces() {
       const double rhoi = Org->rho; 
       const double rhoj = Dest->rho;
-
       const double Vi = mi/rhoi; 
       const double Vj = mj/rhoj;
-      
       assert(Vi>0.0);
       assert(Vj>0.0);
       const double Vi2 = Vi*Vi; 
       const double Vj2 = Vj*Vj;
       const double shear_rij = 2.0*etai*etaj/(etai + etaj);
-      
       const Vec2d Uij = Org->U - Dest->U;
-
       const double pi = Org->p; 
       const double pj = Dest->p;
       /// viscous forces
       const Vec2d dPdti_visc = -shear_rij*Fij*(Vi2 + Vj2) * Uij;
-
       /// pressure forces
       const Vec2d dPdti_pre = eij*Fij*rij*(pi*Vi2 + pj*Vj2);
-      
       const Vec2d dPdti = dPdti_visc  + dPdti_pre;
-      
       Org->dUdt += dPdti*rmi;
       Dest->dUdt -= dPdti*rmj;
 };
@@ -52,14 +45,9 @@ void InteractionIn::UpdateForces() {
 InteractionIn::InteractionIn(const spParticle prtl_org, const spParticle prtl_dest, 
 	      spKernel weight_function, const double dstc,
 	      const Initiation& ini): 
-  Interaction(prtl_org, prtl_dest, weight_function, dstc, ini)
-{
+  Interaction(prtl_org, prtl_dest, weight_function, dstc, ini) {
   assert(ini.simu_mode == 1);
 };
 
-
-
 InteractionIn::~InteractionIn() {
 }
-
-
