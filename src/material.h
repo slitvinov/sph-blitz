577c0f527cc19ead920765101fe876e8b96b3f48
Litvinov Sergey <slitvinov@gmail.com>
2010-10-15 10:17:01 +0200

    Chnage indention in material.h

--- src/material.h
+++ src/material.h
@@ -13,62 +13,62 @@ class Initiation;
 class Material {
 
 public:
-	///material name string
-	std::string material_name;
+  ///material name string
+  std::string material_name;
 
-	///\brief material type
-	///
-	///- 0: solid;
-	///-  1: liquid;
-	///-  2: gas
-	int material_type;
+  ///\brief material type
+  ///
+  ///- 0: solid;
+  ///-  1: liquid;
+  ///-  2: gas
+  int material_type;
 
   /// material_type
   const int material_no;
 
-	double cv;///<heat capacity
-	double eta;///<(shear) viscosity
-  	double zeta;///<bulk viscosity
-	double gamma; ///<ratio of specific heats (adiabatic index)
+  double cv;///<heat capacity
+  double eta;///<(shear) viscosity
+  double zeta;///<bulk viscosity
+  double gamma; ///<ratio of specific heats (adiabatic index)
 
-	double nu; ///<kinetic viscosity
+  double nu; ///<kinetic viscosity
   
-	double rho0;///<reference density
-	double sound_speed; ///<sound speed
+  double rho0;///<reference density
+  double sound_speed; ///<sound speed
 
-	///constructor
-	/// imaterial is a number of material in input file
-	Material(Initiation &ini, const int number);
+  ///constructor
+  /// imaterial is a number of material in input file
+  Material(Initiation &ini, const int number);
 
-	///show properties
-	void show_properties();
+  ///show properties
+  void show_properties();
 
-	///obtain parameter b0
-	void Set_b0(const double sound);
+  ///obtain parameter b0
+  void Set_b0(const double sound);
 	
-	//equation of states
+  //equation of states
 
-	///get pressure
-	double get_p(const double rho) const;// equation of state for liquids
+  ///get pressure
+  double get_p(const double rho) const;// equation of state for liquids
 
-  	///get pressure
-	double get_p(const double rho, const double e) const;//equation of state for gas dynamics
-	///get rho from pressure
-	double get_rho(const double p) const ;// equation of state for liquids
+  ///get pressure
+  double get_p(const double rho, const double e) const;//equation of state for gas dynamics
+  ///get rho from pressure
+  double get_rho(const double p) const ;// equation of state for liquids
 
-	///get rho from pressure
-	double get_rho(const double p, const double e) const;//equation of state for gas dynamics
-	///get internal energy
-	double get_e(const double T) const;
-	///get temperature 
-	double get_T(const double e) const;//(function is defined nowhere)
+  ///get rho from pressure
+  double get_rho(const double p, const double e) const;//equation of state for gas dynamics
+  ///get internal energy
+  double get_e(const double T) const;
+  ///get temperature 
+  double get_T(const double e) const;//(function is defined nowhere)
 
-	///get temperature 
-	double get_T(const double p, const double rho) const;
-	///get sound speed
-        double get_Cs() const;
-	///get sound speed
-        double get_Cs(const double p, const double rho) const;
+  ///get temperature 
+  double get_T(const double p, const double rho) const;
+  ///get sound speed
+  double get_Cs() const;
+  ///get sound speed
+  double get_Cs(const double p, const double rho) const;
 
 private:
   double b0;///<reference pressure
