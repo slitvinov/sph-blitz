26d4c793c4e4b9ec816f336fc6449f8dcdefefa3
Litvinov Sergey <slitvinov@gmail.com>
2010-10-19 12:51:12 +0200

    Add program_options static library

--- src/tools/SPHProbe/sphprobe.cpp
+++ src/tools/SPHProbe/sphprobe.cpp
@@ -1,4 +1,12 @@
 #include <iostream>
 int main() {
+  /// read input files
+  /// read parameters of the kernel
+  ///    cutoff, kernell type
+  ///    times t1, t2
+  /// create a cell structure  (no boundary)
+  /// project to the grid
+  /// approximate in time
+  /// output 
   return 0;
 }
