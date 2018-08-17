c9a5e591cddf8eca762cdc62a41ccad53d109e33
Sergey Litvinov <slitvinov@gmail.com>
2018-08-17 12:02:26 +0200

    better dat2punto.sh

--- scripts/dat2punto.sh
+++ scripts/dat2punto.sh
@@ -1,10 +1,6 @@
-#! /bin/bash
+#!/bin/bash
 
-
-# punto -D 2 -c 4 -G 0:0.2 -s 10 hm 
-
-for file in $(ls -1 prtl[0-9]*.dat); do
-    #printf "file: %s\n" $file > "/dev/stderr"
-    awk 'NF>3&&NR>3{print $1, $2, $3, $4}'  $file 
-    printf "\n"
+for i in prtl[0-9]*.dat
+do awk 'NF>3 && NR>3 {print $1, $2, $3, $4}' "$i"
+   printf '\n'
 done 
