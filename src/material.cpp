bafa123e0e31318782151a5cc78830bdfaca44e9
Sergey Litvinov <slitvinov@gmail.com>
2018-08-17 17:22:18 +0200

    do not print b0

--- src/material.cpp
+++ src/material.cpp
@@ -100,7 +100,6 @@ void Material::show_properties()
 	cout<<"Material: "<<material_name<<"\n";		
 	cout<<"The viscosity is "<<eta<<" Pa.s \n";
 	cout<<"The heat ratio is "<<gamma<<"\n";
-	cout<<"The reference pressure b0 is "<<b0<<" Pa\n";
 	cout<<"The reference density is "<<rho0<<" kg/m^3 \n";
 	cout<<"The premited compressibilty a0 is "<<1.0/a0*100.0<<" percent\n";
 }
