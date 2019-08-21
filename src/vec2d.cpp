1aef5c8b705f83872005dcbf895fb7fc479a83e5
Sergey Litvinov <slitvinov@gmail.com>
2019-08-21 03:59:51 +0200

    minor

--- src/vec2d.cpp
+++ src/vec2d.cpp
@@ -1,15 +1,5 @@
-// vec2d.cpp
-// author: Xiangyu Hu <Xiangyu.Hu@aer.mw.tum.de>
-// changes by: Martin Bernreuther <Martin.Bernreuther@ipvs.uni-stuttgart.de>,
-
-//----------------------------------------------------------------------------------------
-//      Define 2-d vectors and associated operations
-//		Vec2d.cpp
-//----------------------------------------------------------------------------------------
-
 #include <cmath>
 #include "vec2d.h"
-
 
 Vec2d::Vec2d(){
     enum {X, Y};
@@ -23,7 +13,7 @@ Vec2d::Vec2d(double xval , double yval){
 }
 
 Vec2d::Vec2d(const Vec2d &v){
-  enum {X, Y};
-  D[X] = v[X];
-  D[Y] = v[Y];
+    enum {X, Y};
+    D[X] = v[X];
+    D[Y] = v[Y];
 }
