aa9087c845c335a7b51bb1b4949336b59028ee0b
Sergey Litvinov <slitvinov@gmail.com>
2019-08-23 01:01:57 +0200

    minor

--- src/volume.cpp
+++ src/volume.cpp
@@ -11,31 +11,31 @@
 void VolumeMass(Hydrodynamics *hydro, ParticleManager *particles, QuinticSpline *weight_function)
 {
     enum {X, Y};
-    double reciprocV; 
+    double reciprocV;
     double dstc;
     double eij[2], sumdw[2];
     LlistNode<Particle> *p, *p1;
     Particle *prtl_org, *prtl_dest;
-    for (p = hydro.particle_list.first(); 
-	 !hydro.particle_list.isEnd(p); 
-	 p = hydro.particle_list.next(p)) {
-	prtl_org= hydro.particle_list.retrieve(p);
-	particles.BuildNNP(prtl_org->R);
+    for (p = hydro->particle_list.first();
+	 !hydro->particle_list.isEnd(p);
+	 p = hydro->particle_list.next(p)) {
+	prtl_org= hydro->particle_list.retrieve(p);
+	particles->BuildNNP(prtl_org->R);
 	reciprocV = 0.0; sumdw[X] = sumdw[Y] = 0.0;
-	for (p1 = particles.NNP_list.first(); 
-	     !particles.NNP_list.isEnd(p1); 
-	     p1 = particles.NNP_list.next(p1)) {
-	    prtl_dest = particles.NNP_list.retrieve(p1);
+	for (p1 = particles->NNP_list.first();
+	     !particles->NNP_list.isEnd(p1);
+	     p1 = particles->NNP_list.next(p1)) {
+	    prtl_dest = particles->NNP_list.retrieve(p1);
 	    dstc = vv_distance(prtl_org->R, prtl_dest->R);
 	    eij[X] = (prtl_org->R[X] - prtl_dest->R[X])/(dstc + 1.e-30);
 	    eij[Y] = (prtl_org->R[Y] - prtl_dest->R[Y])/(dstc + 1.e-30);
-	    reciprocV += w(&weight_function, dstc);
-	    sumdw[X] += eij[X]*F(&weight_function, dstc);
-	    sumdw[Y] += eij[Y]*F(&weight_function, dstc);	    
+	    reciprocV += w(weight_function, dstc);
+	    sumdw[X] += eij[X]*F(weight_function, dstc);
+	    sumdw[Y] += eij[Y]*F(weight_function, dstc);
 	}
 	reciprocV = 1.0/reciprocV;
 	prtl_org->V = reciprocV;
 	prtl_org->m = prtl_org->rho*reciprocV;
-	particles.NNP_list.clear();
+	particles->NNP_list.clear();
     }
 }
