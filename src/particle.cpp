6d30e9765f36f991c231e28f6c38db0a5b342be0
Sergey Litvinov <slitvinov@gmail.com>
2019-08-24 00:37:31 +0200

    minor

--- src/particle.cpp
+++ src/particle.cpp
@@ -9,8 +9,8 @@ enum {X, Y};
 long particle_ID_max;
 int particle_number_of_materials;
 
-#define AA(v) q->v = p->v
-#define A(v) v = q->v
+#define AA(v) q->v = s->v
+#define A(v) v = s->v
 #define B(a, b) a = b
 #define C(a, b) a = b
 
@@ -88,15 +88,15 @@ Particle::Particle(double x, double y, Material *material)
     B(T, 0.0);
 }
 
-Particle::Particle(Particle *q)
+Particle::Particle(Particle *s)
 {
     B(bd, 1);
     B(bd_type, 1);
     B(ID, 0);
-    B(rl_prtl, q);
+    B(rl_prtl, s);
     A(mtl);
-    B(eta, mtl->eta);
-    B(zeta, mtl->zeta);
+    C(eta, mtl->eta);
+    C(zeta, mtl->zeta);
     A(R[X]);
     A(R[Y]);
     A(rho);
@@ -120,7 +120,7 @@ Particle::Particle(Particle *q)
     A(P[Y]);
     A(P_I[X]);
     A(P_I[Y]);
-    rho_I = rho;
+    C(rho_I, rho);
     A(P_n[X]);
     A(P_n[Y]);
     A(U_n[X]);
@@ -129,12 +129,12 @@ Particle::Particle(Particle *q)
     B(phi, phi_ini());
 }
 
-Particle::Particle(Particle *q, Material *material)
+Particle::Particle(Particle *s, Material *material)
 {
     B(bd, 1);
     B(bd_type, 0);
     B(ID, 0);
-    B(rl_prtl, q);
+    B(rl_prtl, s);
     B(mtl, material);
     A(eta);
     A(zeta);
@@ -161,7 +161,7 @@ Particle::Particle(Particle *q, Material *material)
     A(P[Y]);
     A(P_I[X]);
     A(P_I[Y]);
-    B(rho_I, rho);
+    C(rho_I, rho);
     A(P_n[X]);
     A(P_n[Y]);
     A(U_n[X]);
@@ -170,7 +170,7 @@ Particle::Particle(Particle *q, Material *material)
     B(phi, phi_ini());
 }
 
-void particle_copy(Particle *q, Particle *p, int type)
+void particle_copy(Particle *q, Particle *s, int type)
 {
     int i, j;
     AA(R[X]);
@@ -202,6 +202,6 @@ void particle_copy(Particle *q, Particle *p, int type)
     if (type == 0) {
 	q->phi[0][0] = 0;
 	for(i = 1; i < particle_number_of_materials; i++)
-	    q->phi[0][0] += p->phi[i][i];
+	    q->phi[0][0] += s->phi[i][i];
     }
 }
