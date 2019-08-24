a5b00166796e3ac1b5ff666100632edee3796995
Sergey Litvinov <slitvinov@gmail.com>
2019-08-24 15:56:04 +0200

    LOOP1 in volume

--- src/volume.cpp
+++ src/volume.cpp
@@ -10,6 +10,14 @@
 #include "particlemanager.h"
 #include "hydrodynamics.h"
 #include "volume.h"
+
+#define LOOP(q, l) for (p = l.first();					\
+    			!list_isEnd(&l, p) && (q = l.retrieve(p), 1);	\
+			p = l.next(p))
+#define LOOP1(q, l) for (p1 = l.first();				\
+			 !list_isEnd(&l, p1) && (q = l.retrieve(p1), 1); \
+			 p1 = l.next(p))
+
 void VolumeMass(Hydrodynamics *hydro, ParticleManager *particles, QuinticSpline *weight_function)
 {
     enum {X, Y};
@@ -18,16 +26,10 @@ void VolumeMass(Hydrodynamics *hydro, ParticleManager *particles, QuinticSpline 
     double eij[2], sumdw[2];
     ListNode *p, *p1;
     Particle *prtl_org, *prtl_dest;
-    for (p = hydro->particle_list.first();
-	 !list_isEnd(&hydro->particle_list, p);
-	 p = hydro->particle_list.next(p)) {
-	prtl_org= hydro->particle_list.retrieve(p);
+    LOOP(prtl_org, hydro->particle_list) {
 	particles->BuildNNP(prtl_org->R);
 	reciprocV = 0.0; sumdw[X] = sumdw[Y] = 0.0;
-	for (p1 = particles->NNP_list.first();
-	     !list_isEnd(&particles->NNP_list, p1);
-	     p1 = particles->NNP_list.next(p1)) {
-	    prtl_dest = particles->NNP_list.retrieve(p1);
+	LOOP1(prtl_dest, particles->NNP_list) {
 	    dstc = vv_distance(prtl_org->R, prtl_dest->R);
 	    eij[X] = (prtl_org->R[X] - prtl_dest->R[X])/(dstc + 1.e-30);
 	    eij[Y] = (prtl_org->R[Y] - prtl_dest->R[Y])/(dstc + 1.e-30);
