e9e7d8893b67c4a74989f5e1ea02fb1b57093879
Sergey Litvinov <slitvinov@gmail.com>
2019-08-25 09:40:18 +0200

    minor

--- src/mls.h
+++ src/mls.h
@@ -2,14 +2,14 @@ class Initiation;
 class Particle;
 class QuinticSpline;
 class MLS {
-	int N;
-	int MLS_MAX; 
-	double A[3][3], **B; 
-	double p[3], **pi; 
-	double *Wi; 
-	void MLS_Solver(int order);
+    int N;
+    int MLS_MAX;
+    double A[3][3], **B;
+    double p[3], **pi;
+    double *Wi;
+    void MLS_Solver(int order);
 public:
-	double *phi; 
-	explicit MLS(int MLS_MAX);
-	void MLSMapping(double point[2], List *NNP_list, QuinticSpline *weight_function, int order);
+    double *phi;
+    explicit MLS(int MLS_MAX);
+    void MLSMapping(double point[2], List *NNP_list, QuinticSpline *weight_function, int order);
 };
