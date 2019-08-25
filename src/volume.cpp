be3fb547a7ff6d1f4e8dd6c8596d0944e1a2c32b
Sergey Litvinov <slitvinov@gmail.com>
2019-08-25 15:07:43 +0200

    unuse var

--- src/volume.cpp
+++ src/volume.cpp
@@ -17,7 +17,6 @@ void VolumeMass(List *particle_list, ParticleManager *particles, QuinticSpline *
     enum {X, Y};
     double reciprocV;
     double dstc;
-    double eij[2];
     ListNode *p, *p1;
     Particle *prtl_org, *prtl_dest;
     LOOP_P(prtl_org, particle_list) {
@@ -25,8 +24,6 @@ void VolumeMass(List *particle_list, ParticleManager *particles, QuinticSpline *
 	reciprocV = 0.0;
 	LOOP1_P(prtl_dest, particles->NNP_list) {
 	    dstc = vv_distance(prtl_org->R, prtl_dest->R);
-	    eij[X] = (prtl_org->R[X] - prtl_dest->R[X])/(dstc + 1.e-30);
-	    eij[Y] = (prtl_org->R[Y] - prtl_dest->R[Y])/(dstc + 1.e-30);
 	    reciprocV += w(weight_function, dstc);
  	}
 	reciprocV = 1.0/reciprocV;
