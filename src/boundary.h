0d1994fdef17e7610237b361338df5401083322a
Sergey Litvinov <slitvinov@gmail.com>
2019-08-27 12:00:12 +0200

    sort vars

--- src/boundary.h
+++ src/boundary.h
@@ -5,24 +5,23 @@ extern "C" {
     struct Manager;
     struct Boundary {
 	double box_size[2];
-	int x_clls;
-	int y_clls;
-	int xBl;
-	int xBr;
-	int yBd;
-	int yBu;
 	double UxBl[2];
 	double UxBr[2];
 	double UyBd[2];
 	double UyBu[2];
+	int xBl;
+	int xBr;
+	int x_clls;
+	int yBd;
+	int yBu;
+	int y_clls;
 	struct List *b;
     };
 
     struct Boundary *boundary_ini(struct Initiation *);
     int boundary_fin(struct Boundary *);
     int boundary_condition(struct Boundary *, struct List ***);
-    int boundary_build(struct Boundary *, struct List ***,
-		       struct Material *);
+    int boundary_build(struct Boundary *, struct List ***, struct Material *);
     int boundary_check(struct Boundary *, struct List *);
     int boundary_w(struct Boundary *, struct Particle *);
     int boundary_e(struct Boundary *, struct Particle *);
