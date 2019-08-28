a77c3570eb96f31bdb7756a77478db2509d27da3
Sergey Litvinov <slitvinov@gmail.com>
2019-08-28 15:24:05 +0200

    upd inv.c

--- example/inv.c
+++ example/inv.c
@@ -1,3 +1,22 @@
+#include <stdio.h>
 #include <glbfunc.h>
-int main() {
+enum { n = 3 };
+int
+main()
+{
+    int i, j, k;
+
+    /* m : matrix([1, 2, 3], [2, 4, 5], [3, 5, 6]); */
+    double a[] = { 1, 2, 3,
+	2, 4, 5,
+	3, 5, 6
+    };
+    SymmetricInverse3x3(a);
+    for (k = i = 0; i < n; i++) {
+	for (j = 0; j < n; j++) {
+	    if (j > 0) putchar(' ');
+	    printf("%g", a[k++]);
+	}
+	putchar('\n');
+    }
 }
