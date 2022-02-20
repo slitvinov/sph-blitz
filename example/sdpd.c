a8147eebd4e30b69f7aff4afd5aee9a1bf2adc5e
Sergey Litvinov <slitvinov@gmail.com>
2022-02-20 12:18:14 -0500

    rm step.c

--- example/sdpd.c
+++ example/sdpd.c
@@ -6,7 +6,6 @@
 #include "sph/material.h"
 #include "sph/pair.h"
 #include "sph/particle.h"
-#include "sph/step.h"
 extern double pair_art_vis;
 extern double pair_delta;
 extern long particle_ID_max;
--- src/sph/step.h
+++
@@ -1,4 +0,0 @@
-struct Ini;
-struct Kernel;
-void step(int *ite, struct Ini *,
-          double *, double D_time, struct Kernel *);
